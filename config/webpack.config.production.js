83474743e9f88aaa3089b9e538c1e42bf31f04cc
baolq <luoqiang.bao@gmail.com>
2019-12-26 17:30:26 +0800

    feat: update pkg

--- config/webpack.config.production.js
+++ config/webpack.config.production.js
@@ -2,11 +2,13 @@
 const path = require('path')
 const moment = require('moment')
 const webpack = require('webpack')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackCleanupPlugin = require('webpack-cleanup-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 
 const rules = require('./webpack.rules')
 module.exports = {
@@ -19,7 +21,7 @@ module.exports = {
     library: 'someLibName',
     libraryTarget: 'umd'
   },
-  externals : {
+  externals: {
     react: 'react',
     'react-dom': 'react-dom',
     'prop-types': 'prop-types',
@@ -38,6 +40,7 @@ module.exports = {
         }
       }),
       new OptimizeCSSAssetsPlugin({}),
+      new BundleAnalyzerPlugin(),
       new webpack.BannerPlugin(`${moment().format('YYYY-MM-DD HH:mm:ss')}`)
     ]
   },
@@ -45,98 +48,100 @@ module.exports = {
     modules: ['node_modules', 'src']
   },
   module: {
-    rules: rules.concat([{
-      test: /\.jsx?$/,
-      use: ['babel-loader'],
-      exclude: /node_modules/
-    },
-    {
-      test: /\.css$/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader'
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            config: {
-              path: 'config/postcss.config.js'
+    rules: rules.concat([
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              config: {
+                path: 'config/postcss.config.js'
+              }
             }
           }
-        }
-      ]
-    },
-    {
-      test: /\.less$/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader'
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            config: {
-              path: 'config/postcss.config.js'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              config: {
+                path: 'config/postcss.config.js'
+              }
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              relativeUrls: false,
+              javascriptEnabled: true
             }
           }
-        },
-        {
-          loader: 'less-loader',
-          options: {
-            relativeUrls: false,
-            javascriptEnabled: true
-          }
-        }
-      ]
-    },
-    {
-      test: /\.(jpe?g|png|gif)$/,
-      loader: 'url-loader',
-      options: {
-        // Inline files smaller than 10 kB (10240 bytes)
-        limit: 10 * 1024,
-        name: 'image/[hash].[ext]'
-      }
-    },
-    {
-      test: /\.svg$/,
-      loader: 'svg-url-loader',
-      options: {
-        // Inline files smaller than 10 kB (10240 bytes)
-        limit: 10 * 1024,
-        name: 'image/[hash].[ext]',
-        // Remove the quotes from the url
-        // (they’re unnecessary in most cases)
-        noquotes: true
-      }
-    },
-    {
-      test: /\.(jpe?g|png|gif|svg)$/,
-      loader: 'image-webpack-loader',
-      options: {
-        mozjpeg: {
-          quality: 80
-        },
-        // optipng.enabled: false will disable optipng
-        optipng: {
-          enabled: false
-        },
-        pngquant: {
-          quality: '65-90',
-          speed: 4
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          // Inline files smaller than 10 kB (10240 bytes)
+          limit: 10 * 1024,
+          name: 'image/[hash].[ext]'
         }
       },
-      // This will apply the loader before the other ones
-      enforce: 'pre'
-    }])
+      {
+        test: /\.svg$/,
+        loader: 'svg-url-loader',
+        options: {
+          // Inline files smaller than 10 kB (10240 bytes)
+          limit: 10 * 1024,
+          name: 'image/[hash].[ext]',
+          // Remove the quotes from the url
+          // (they’re unnecessary in most cases)
+          noquotes: true
+        }
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        loader: 'image-webpack-loader',
+        options: {
+          mozjpeg: {
+            quality: 80
+          },
+          // optipng.enabled: false will disable optipng
+          optipng: {
+            enabled: false
+          },
+          pngquant: {
+            quality: '65-90',
+            speed: 4
+          }
+        },
+        // This will apply the loader before the other ones
+        enforce: 'pre'
+      }
+    ])
   },
   plugins: [
     new WebpackCleanupPlugin(),
     new webpack.DefinePlugin({}),
     new webpack.ProvidePlugin({
-      'React': 'react'
+      React: 'react'
     }),
     new MiniCssExtractPlugin({
       chunkFilename: '[name].[hash].css',
