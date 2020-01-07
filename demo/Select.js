f2d438d65b40885963b46392b59cd46de0800ded
baolq <luoqiang.bao@gmail.com>
2020-01-07 16:00:30 +0800

    feat:  添加测试用例

---
+++ demo/Select.js
@@ -0,0 +1,45 @@
+import React from "react";
+import SuperSelect from "../packages/VirtualSelect";
+
+const Option = SuperSelect.Option;
+
+const children = [];
+
+for (let i = 0; i < 10000; i++) {
+  children.push(
+    <Option value={i + "aa"} key={i}>
+      {i}
+    </Option>
+  );
+}
+
+class App extends React.Component {
+  onChange = v => {
+    console.log(v, "onChange");
+  };
+
+  onSearch = v => console.log(v);
+
+  render() {
+    return (
+      <div>
+        <div style={{ width: "300px" }}>
+          superSelect: 所有用法同 antd 原 Select, 只是替换 Select 为 SuperSelect
+          <SuperSelect
+            showSearch
+            allowClear
+            open
+            onChange={this.onChange}
+            onSearch={this.onSearch}
+            style={{ width: "300px" }}
+            getPopupContainer={target => target.parentNode}
+          >
+            {children}
+          </SuperSelect>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ test/index.spec.js
@@ -0,0 +1,22 @@
+import { mount, shallow } from "enzyme";
+
+import Lib from "../lib";
+import SelectDemo from "../demo/Select";
+
+let wrappedComponent = <SelectDemo />;
+
+test("lib", () => {
+  const wrapper = mount(<Lib />);
+
+  expect(wrapper.html()).toBeTruthy();
+});
+
+test("should render correctly", () => {
+  const wrapper = mount(wrappedComponent);
+  expect(wrapper.html()).toBeTruthy();
+});
+
+// test("demo should matchSnapshot", () => {
+//   const tree = renderer.create(wrappedComponent);
+//   expect(tree).toMatchSnapshot();
+// });
